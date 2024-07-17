291ff5237f4da89b3015f48c3dea3dc129f53e67
Nicolas Queiroz <nicolas.queirozvf@gmail.com>
2024-07-17 16:47:36 -0400

    feat: adding Keyboard buttons

--- calculator-react/src/components/Keyboard/Keyboard.tsx
+++ calculator-react/src/components/Keyboard/Keyboard.tsx
@@ -1,8 +1,26 @@
 import './Keyboard.css'
 
 const Keyboard = () => {
+
   return (
     <div className='calculator-keyboard'>
+      <button className='grid-item-operator'>+</button>
+      <button className='grid-item-operator'>-</button>
+      <button className='grid-item-operator'>*</button>
+      <button className='grid-item-operator'>/</button>
+      <button className='grid-item-number'>7</button>
+      <button className='grid-item-number'>8</button>
+      <button className='grid-item-number'>9</button>
+      <button className='grid-item-calc'>=</button>
+      <button className='grid-item-number'>4</button>
+      <button className='grid-item-number'>5</button>
+      <button className='grid-item-number'>6</button>
+      <button className='grid-item-number'>1</button>
+      <button className='grid-item-number'>2</button>
+      <button className='grid-item-number'>3</button>
+      <button className='grid-item-number'>0</button>
+      <button className='grid-item-number'>C</button>
+      <button className='grid-item-number'>.</button>
     </div>
   )
 }
