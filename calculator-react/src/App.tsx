294ab9e63d7ba1e776d9c5fe074d00e153b8222a
Nicolas Queiroz <nicolas.queirozvf@gmail.com>
2024-07-17 20:16:34 -0400

    feat: validating decimals

--- calculator-react/src/App.tsx
+++ calculator-react/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Display, Keyboard } from './components';
 
-import { MATH_OPERATOR, MATH_OPERATORS } from './constants';
+import { DECIMAL_LENGTH, MATH_OPERATOR, MATH_OPERATORS } from './constants';
 import './App.css'
 
 function App() {
@@ -11,27 +11,44 @@ function App() {
     let result = 0;
     const mathOperator = MATH_OPERATORS.filter(item => expression.includes(item))[0]
 
-    const [num1, num2] = expression.split(mathOperator);
+    const [num1Str, num2Str] = expression.split(mathOperator);
+
+    const num1 = parseInt(num1Str, 10)
+    const num2 = parseInt(num2Str, 10)
+
     switch (mathOperator) {
       case MATH_OPERATOR.add:
         {
-          result = parseInt(num1, 10) + parseInt(num2, 10);
+          result = num1 + num2;
           break;
         }
       case MATH_OPERATOR.subtract:
         {
-          result = parseInt(num1, 10) - parseInt(num2, 10);
+          result = num1 - num2;
           break;
         }
       case MATH_OPERATOR.multiply:
         {
-          result = parseInt(num1, 10) * parseInt(num2, 10);
+          result = num1 * num2;
           break;
         }
       case MATH_OPERATOR.divide:
         {
-          // TODO: validate whem dem is `0`
-          result = parseInt(num1, 10) / parseInt(num2, 10);
+          if (num2 !== 0) {
+            result = num1 / num2;
+            const [, fraction] = result.toString().split(".")
+            if (fraction) {
+              if (fraction.length > DECIMAL_LENGTH) {
+                result = parseFloat(result.toFixed(DECIMAL_LENGTH))
+              }
+              else {
+                result = parseFloat(result.toFixed(fraction.length))
+              }
+            }
+          }
+          else {
+            result = NaN;
+          }
           break;
         }
     }
