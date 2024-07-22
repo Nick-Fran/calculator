22eae4504f8fbf3e8409d4295889effe8f1bf365
Nicolas Queiroz <nicolas.queirozvf@gmail.com>
2024-07-21 23:05:28 -0400

    feat: implementing UI on Keyboard component

--- calculator-angular/src/app/app.component.ts
+++ calculator-angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DisplayComponent } from "./display/display.component";
+import { KeyboardComponent } from "./keyboard/keyboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DisplayComponent, KeyboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
